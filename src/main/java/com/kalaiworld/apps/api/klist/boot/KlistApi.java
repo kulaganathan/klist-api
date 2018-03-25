4eaf3b2ca9f72c68f9c53501ba4cd2b0478f06a6
kalai <eie.kalai@gmail.com>
2018-03-25 14:04:49 -0500

    barebone working setup

---
+++ src/main/java/com/kalaiworld/apps/api/klist/boot/KlistApi.java
@@ -0,0 +1,11 @@
+package com.kalaiworld.apps.api.klist.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlistApi {
+  public static void main(String[] args){
+    SpringApplication.run(KlistApi.class,args);
+  }
+}
